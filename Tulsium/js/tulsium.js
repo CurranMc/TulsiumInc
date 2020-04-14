536e293fd7d5bfe671950bc66ae004ed852cf7a3
CurranMc <38260264+CurranMc@users.noreply.github.com>
2020-04-14 00:33:09 -0700

    Starting code and folderpaths for the Project
    these are the folder paths for this project
    the stylesheet named style will be the father styling sheet all html files will follow that one

---
+++ Tulsium/js/tulsium.js
@@ -0,0 +1,28 @@
+function arizonaInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Arizona</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
+}
+function caliInfo(){
+    document.getElementById('stateName').innerHTML = "<p>California</p>";
+    document.getElementById('infoText').innerHTML = "<p>We work in this area, we are based around San Jose. Let us know how we can help in this area</p>";
+}
+function idahoInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Idaho</p>";
+    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+}
+function nevadaInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Nevada</p>";
+    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+}
+function oregonInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Oregon</p>";
+    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+}
+function utahInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Utah</p>";
+    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+}
+function washingtonInfo(){
+    document.getElementById('stateName').innerHTML = "<p>Washington</p>";
+    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+}
