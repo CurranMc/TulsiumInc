9e6412677c79f8515653b7e59431898b7f91b89a
CurranMc <38260264+CurranMc@users.noreply.github.com>
2020-04-18 02:10:50 -0700

    lil update

--- Tulsium/js/tulsium.js
+++ Tulsium/js/tulsium.js
@@ -8,21 +8,21 @@ function caliInfo(){
 }
 function idahoInfo(){
     document.getElementById('stateName').innerHTML = "<p>Idaho</p>";
-    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
 }
 function nevadaInfo(){
     document.getElementById('stateName').innerHTML = "<p>Nevada</p>";
-    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
 }
 function oregonInfo(){
     document.getElementById('stateName').innerHTML = "<p>Oregon</p>";
-    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
 }
 function utahInfo(){
     document.getElementById('stateName').innerHTML = "<p>Utah</p>";
-    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
 }
 function washingtonInfo(){
     document.getElementById('stateName').innerHTML = "<p>Washington</p>";
-    document.getElementById('infoText').innerHTML = "<p>Your Plant Has Died there wasn't enough sun it couldn't start photosynthesis</p>";
+    document.getElementById('infoText').innerHTML = "<p>We currently do not have anyone in this area. However, we are planning to expand to here! Nevertheless, let us know as we maybe able to send someone out there</p>";
 }
